d9340d65aa2e20fce0cb869fc2e9a17c760c65c1
MarianaOliveiraFelipe <107776071+MarianaOliveiraFelipe@users.noreply.github.com>
2024-03-24 19:08:42 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+//import Helloworld from "./components/01HelloWorld/HelloWorld";
+//import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
+//import Loja from "./components/03Props/Loja";
+//import Curso from "./components/03Props/Curso";
+//import './components/atividade00/estilos.css';
+//import {MeusDados1 as Md, MeusDados2 as Md2} from "./components/00Atividade/atividade00/MeusDados1";
+//import MeusDados2 from "./components/00Atividade/atividade00/MeusDados2";
+//import Temperatura from "./components/00Atividade/atividade00/Temperatura";
+//import SistemaSolar from "./components/04Children/SistemaSolar";
+//import Planetas from "./components/04Children/Planetas";
+//import Galaxia from "./components/04Children/Galaxia";
+//import Pai from "./components/04Children/Pai";
+//import Estado from "./components/06Estados/Estado";
+//import Pokemon from "./components/06Estados/Pokemon";
+//import { CompA } from "./components/06Estados/07PropDrilling/PropDrilling";
+//import { CompA } from "./components/08Contexto/Contexto";
+//import PokemonPrincipal from "./components/09PokemonContexto/PokemonPrincipal";
+//import Pai from "./components/00Atividade/atividade01/questao01/01Pai";
+//import MeuPc from "./components/00Atividade/atividade01/02MeuPc";
+//import  { World, Arena } from "./components/00Atividade/atividade01/03Batalha";
+import ClassA from "./components/00Atividade/atividade02/questao01/ClassContext";
+import ComponenteAvo from "./components/00Atividade/atividade02/questao02/ComponenteAvo";
+import ComponenteAvoEstado from "./components/00Atividade/atividade02/questao03/ComponenteAvoEstado";
+function App(){
+
+  return(
+    <div> 
+      <h1>Questão 01</h1>
+      <hr />
+      <ClassA />
+      <hr />
+      <h1> Questão 02</h1>
+      <ComponenteAvo />
+      <hr />
+      <h1>Questão 03</h1>
+      <ComponenteAvoEstado />
+
+      
+    </div>
+  )
+}
+
+
+export default App;
+
+
+ 
